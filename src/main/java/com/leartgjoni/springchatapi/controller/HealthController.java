6ffcda2450eb11810c8298ac9f8b90f06edc5fef
Leart Gjoni <leartemail@gmail.com>
2021-02-28 19:54:54 -0500

    feat: health endpoint (#4)

---
+++ src/main/java/com/leartgjoni/springchatapi/controller/HealthController.java
@@ -0,0 +1,12 @@
+package com.leartgjoni.springchatapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "healthy";
+    }
+}
